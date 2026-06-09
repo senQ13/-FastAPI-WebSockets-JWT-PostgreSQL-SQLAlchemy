b49e1e9d1aa5fbedcb3c05bad151a4b806069c66
Senq13 <neko20840@gmail.com>
2026-06-10 07:25:27 +1000

    fix: add DATABASE_URL validation

--- db.py
+++ db.py
@@ -4,5 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_async_engine(DATABASE_URL)
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL не задан в переменных окружения")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
